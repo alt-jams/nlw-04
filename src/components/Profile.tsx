2b792ebb5711a7947cb90efea52208a9e713360c
Jamilly Cuco <jamillysantos2507@gmail.com>
2021-02-25 17:02:30 -0300

    day 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/challengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/alt-jams.png" alt="Jamilly Cuco"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong> Jamilly Cuco</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
