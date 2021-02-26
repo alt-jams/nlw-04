15ef7d12d6508f30b07173853bd0bb7f98809cff
Jamilly Cuco <jamillysantos2507@gmail.com>
2021-02-26 20:57:37 -0300

    fix dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import '../styles/pages/global.css'
 
 export default class MyDocument extends Document {
     render() {
@@ -11,7 +10,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
                 </Head>
-                <body className="light">
+                <body>
                     <Main />
                     <NextScript />
                 </body>
